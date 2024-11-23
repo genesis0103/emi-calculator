8e13b66a69a41008c7b09cfc04b1d9e82b688d25
Ghazi Khan <ghazikhan205@gmail.com>
2024-11-23 21:20:12 +0530

    Calculator created for EMIs

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Calculator } from "@/components/wrapper/Calculator.tsx";
+
+function App() {
+  return (
+    <div className="w-full p-10 h-screen">
+      <h1 className="text-2xl text-center font-bold mb-10">EMI Calculator</h1>
+
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
